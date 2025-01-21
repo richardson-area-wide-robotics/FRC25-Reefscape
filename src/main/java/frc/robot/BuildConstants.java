4bf5cf6fdad7040f6f54e5cc49a6aa76980b990d
at384752 <at384752@g.risd.org>
2025-01-21 09:08:01 -0600

    Updated dependencies

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC25-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 21;
-  public static final String GIT_SHA = "c64ad4a9853867786d5dedb3d14dfa6706ae892c";
-  public static final String GIT_DATE = "2025-01-19 00:13:16 CST";
+  public static final int GIT_REVISION = 22;
+  public static final String GIT_SHA = "e1586ce71088467c6c2d5491479e16dd1e1e4809";
+  public static final String GIT_DATE = "2025-01-19 17:42:19 CST";
   public static final String GIT_BRANCH = "john-deploy";
-  public static final String BUILD_DATE = "2025-01-19 17:34:32 CST";
-  public static final long BUILD_UNIX_TIME = 1737329672850L;
+  public static final String BUILD_DATE = "2025-01-21 09:07:40 CST";
+  public static final long BUILD_UNIX_TIME = 1737472060237L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
