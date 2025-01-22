3fb8a8c1c6da74b6e90569b2bc5ea6be0362ba71
at384752 <at384752@g.risd.org>
2025-01-21 18:47:17 -0600

    Changed CAN Drive IDs

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC25-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 22;
-  public static final String GIT_SHA = "e1586ce71088467c6c2d5491479e16dd1e1e4809";
-  public static final String GIT_DATE = "2025-01-19 17:42:19 CST";
+  public static final int GIT_REVISION = 23;
+  public static final String GIT_SHA = "4bf5cf6fdad7040f6f54e5cc49a6aa76980b990d";
+  public static final String GIT_DATE = "2025-01-21 09:08:01 CST";
   public static final String GIT_BRANCH = "john-deploy";
-  public static final String BUILD_DATE = "2025-01-21 09:07:40 CST";
-  public static final long BUILD_UNIX_TIME = 1737472060237L;
+  public static final String BUILD_DATE = "2025-01-21 16:39:56 CST";
+  public static final long BUILD_UNIX_TIME = 1737499196967L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
