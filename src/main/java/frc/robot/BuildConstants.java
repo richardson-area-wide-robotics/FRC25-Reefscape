b924acba9ded1bcd738a52cf39882f68a9ef8371
at384752 <at384752@g.risd.org>
2025-02-11 11:34:20 -0600

    Added autons (missing paths)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC25-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 40;
-  public static final String GIT_SHA = "261e66c5d3e52d0669193a032e690b3a5336547a";
-  public static final String GIT_DATE = "2025-02-11 09:37:15 CST";
+  public static final int GIT_REVISION = 41;
+  public static final String GIT_SHA = "7350a6679604d1798e8b7bccd276c883e41ede0d";
+  public static final String GIT_DATE = "2025-02-11 09:44:36 CST";
   public static final String GIT_BRANCH = "new-bot";
-  public static final String BUILD_DATE = "2025-02-11 09:42:25 CST";
-  public static final long BUILD_UNIX_TIME = 1739288545296L;
+  public static final String BUILD_DATE = "2025-02-11 11:28:33 CST";
+  public static final long BUILD_UNIX_TIME = 1739294913698L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
