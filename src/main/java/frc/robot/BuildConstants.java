de3adf164bb38c498065c0490832d922487a60a7
at384752 <at384752@g.risd.org>
2025-02-10 18:25:39 -0600

    Working outtake auto command

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC25-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 38;
-  public static final String GIT_SHA = "8077f53c6b076eeaa6e9dd9dbfadc91fcd7747cd";
-  public static final String GIT_DATE = "2025-02-08 17:20:45 CST";
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "b6c87b3d68b317be78e59f2a4ee98f5cf8fc328f";
+  public static final String GIT_DATE = "2025-02-10 17:09:46 CST";
   public static final String GIT_BRANCH = "pathplanner";
-  public static final String BUILD_DATE = "2025-02-10 17:07:34 CST";
-  public static final long BUILD_UNIX_TIME = 1739228854684L;
+  public static final String BUILD_DATE = "2025-02-10 18:21:44 CST";
+  public static final long BUILD_UNIX_TIME = 1739233304973L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
