43117215c06d3078eb3200b37b53006845da780c
strubium <113206902+strubium@users.noreply.github.com>
2025-02-10 20:51:38 -0600

    Update team number

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC25-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 39;
-  public static final String GIT_SHA = "b6c87b3d68b317be78e59f2a4ee98f5cf8fc328f";
-  public static final String GIT_DATE = "2025-02-10 17:09:46 CST";
-  public static final String GIT_BRANCH = "pathplanner";
-  public static final String BUILD_DATE = "2025-02-10 18:21:44 CST";
-  public static final long BUILD_UNIX_TIME = 1739233304973L;
+  public static final int GIT_REVISION = 37;
+  public static final String GIT_SHA = "3352a59d209fc80b1213a3bd03b0b2221b38e4c8";
+  public static final String GIT_DATE = "2025-02-10 19:16:26 CST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-02-10 20:22:13 CST";
+  public static final long BUILD_UNIX_TIME = 1739240533051L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
