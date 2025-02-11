313302b1c3b1367957c19ba5bd4ab1fa9d5a83cb
strubium <113206902+strubium@users.noreply.github.com>
2025-02-10 18:52:07 -0600

    EasyMotor

---
+++ src/main/java/frc/robot/common/EasyMotor.java
@@ -0,0 +1,55 @@
+package frc.robot.common;
+
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class EasyMotor {
+
+    /** Make a {@link SparkMax}
+     *
+     * @param motorID The CAN Bus ID for this motor
+     * @param motorType The type of the motor, Brushless etc
+     * @param idleMode The idle mode of the motor, Break etc
+     *
+     * @author Hudson Strub
+     * @since 2025
+     */
+    public SparkMax createEasySparkMax(int motorID, SparkLowLevel.MotorType motorType, SparkBaseConfig.IdleMode idleMode){
+        SparkMaxConfig config = new SparkMaxConfig();
+        SparkMax motor;
+
+        motor = new SparkMax(motorID, motorType);
+        config.idleMode(idleMode);
+        motor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+        return motor;
+    }
+
+    /** Make a {@link SparkFlex}
+     *
+     * @param motorID The CAN Bus ID for this motor
+     * @param motorType The type of the motor, Brushless etc
+     * @param idleMode The idle mode of the motor, Break etc
+     *
+     * @author Hudson Strub
+     * @since 2025
+     */
+    public SparkFlex createEasySparkFlex(int motorID, SparkLowLevel.MotorType motorType, SparkBaseConfig.IdleMode idleMode){
+        SparkFlexConfig config = new SparkFlexConfig();
+        SparkFlex motor;
+
+        motor = new SparkFlex(motorID, motorType);
+        config.idleMode(idleMode);
+        motor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+        return motor;
+    }
+
+}
--- src/main/java/frc/robot/common/subsystems/shooter/KitBotShooter.java
+++ src/main/java/frc/robot/common/subsystems/shooter/KitBotShooter.java
@@ -1,26 +1,22 @@
 package frc.robot.common.subsystems.shooter;
 
-import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.common.EasyMotor;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class KitBotShooter extends SubsystemBase {
-    SparkMaxConfig config = new SparkMaxConfig();
     SparkMax motor;
 
     public KitBotShooter(int motorID) {
-        motor = new SparkMax(motorID, MotorType.kBrushed);
-        config.idleMode(IdleMode.kCoast);
-        motor.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+        motor = EasyMotor.createEasySparkMax(motorID, MotorType.kBrushed, IdleMode.kCoast);
     }
 
     /**
